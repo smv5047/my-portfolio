63d0c0f75735e7cbce9d5af219097789c66909ce
smv5047 <smv5047@gmail.com>
2019-11-05 18:57:46 -0500

    updated title

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to Steven's Temporary Portfolio
+          Welcome to Steven's Temporary Portfolio!
         </p>
       
       </header>
