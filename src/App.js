05d2ee85c55ce76d9424a412c9892af9d5d39895
smv5047 <smv5047@gmail.com>
2020-02-16 16:54:47 -0500

    updated message

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to Steven's Temporary Portfolio!
-        </p>
-      
+        <p>Welcome to Steven's Temporary Portfolio!!!!</p>
       </header>
     </div>
 
     //Hello
   );
 }
-
-
-
 
 export default App;
