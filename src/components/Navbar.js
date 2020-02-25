3f987f4cf21d3af4d7874dd2245087d5006d0f4e
smv5047 <smv5047@gmail.com>
2020-02-24 21:58:22 -0500

    updated NAV bar further

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -17,9 +17,10 @@ function Navbar() {
             onChange={handleChange}
             aria-label="simple tabs example"
           >
-            <Tab label="Item One" />
-            <Tab label="Item Two" />
-            <Tab label="Item Three" />
+            <Tab label="Home" />
+            <Tab label="About" />
+            <Tab label="Now" />
+            <Tab label="Contact" />
             {/* <Tab label="Item One" {...a11yProps(0)} />
             <Tab label="Item Two" {...a11yProps(1)} />
             <Tab label="Item Three" {...a11yProps(2)} /> */}
@@ -34,11 +35,6 @@ function Navbar() {
         <TabPanel value={value} index={2}>
         Item Three
         </TabPanel> */}
-        <h2>Home</h2>
-        <h2>About</h2>
-        <h2>Resume</h2>
-        <h2>Now</h2>
-        <h2>Blog</h2>
       </div>
     </>
   )
