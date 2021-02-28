d54da7341ae74db80b3fb92b126874119b3ee4a1
smv5047 <smv5047@gmail.com>
2021-02-28 13:31:50 -0500

    blog update

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -3,7 +3,7 @@ import React from "react"
 export default function Blog() {
   return (
     <div>
-      <h4>This is a blog</h4>
+      <h4>This is a blog. Bloggy bloggy blog blog</h4>
     </div>
   )
 }
